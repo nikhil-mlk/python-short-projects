a2090886a1f5a4f7a5833d3af1db89284988a233
nikhil-mlk <Nikhil.mlk19861986@gmail.com>
2026-03-30 21:38:21 -0700

    committing to_do_app program

---
+++ to_do_app/to_do_app.py
@@ -0,0 +1,8 @@
+'''
+Requrirements:
+Req1. System will ask user that how many task you want to enter. According to it loop will run and
+user will enter tasks in LIST
+Req 2. A message will display that will show all the tasks entered by user after adding all the tasks
+Req 3: We continously as user that what he wants to do: 1- Add Task, 2 - Update Task, 3 - Delete Task, 4 - View Tasks. If
+user selects 5 - Exit the user should exit from this continous loop
+'''
