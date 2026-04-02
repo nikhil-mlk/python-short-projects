aa1e52e72a6f49ec31f25befaa73881622e3bdb2
nikhil-mlk <Nikhil.mlk19861986@gmail.com>
2026-04-01 21:55:38 -0700

    committing index_of_first_non_repeating_character.py file

---
+++ string_programs/index_of_first_non_repeating_character.py
@@ -0,0 +1,12 @@
+'''
+Req: Find the index of First Non-Repeating Character in String
+
+Ex: String: swiss
+s occurs more than 1 time
+w occurs 1 time
+i occurs 1 time
+But w occurs first and its index is 1.
+so answer is 1
+'''
+
+
