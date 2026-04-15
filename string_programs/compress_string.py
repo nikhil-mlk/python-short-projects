497fc6dcdff68ca662ae1f62f497a9714e7bf775
nikhil-mlk <nikhil.mlk19861986@gmail.com>
2026-04-15 07:42:28 -0700

    Committing Initial changes

--- string_programs/compress_string.py
+++ string_programs/compress_string.py
@@ -12,15 +12,29 @@ Idea:
 iterate string and place characters in dictionary along with their values (number of time occurence)
 
 '''
-str='aaabbca'
+user_string='aaabbcc'
 final_str=''
+i=0
+j=0
+k=1
+while j<len(user_string):
 
-i=str[0]
-j=str[0]
+    j += 1
 
-k=0
-while k<len(str):
-    pass
+    if user_string[j] == user_string[i]:
+        k += 1
+    elif user_string[j]!=user_string[i]:
+        final_str=final_str+user_string[i]+str(k)
+        i=j
+        k=1
+print(final_str)
+
+
+
+
+
+
+
 
 
 
