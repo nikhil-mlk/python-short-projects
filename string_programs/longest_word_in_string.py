f46f7bff3008916326e2a9ac6ffc348a6e20decf
nikhil-mlk <Nikhil.mlk19861986@gmail.com>
2026-04-14 20:52:46 -0700

    coding is completed, hence commiting the changes

--- string_programs/longest_word_in_string.py
+++ string_programs/longest_word_in_string.py
@@ -1,7 +1,40 @@
 '''
 Requirement:
 We need to find the longest word in a string
-string = i love python programming
+string = python programming is fun
 
 Ans: programming --> length: 11
+
+What we need:
+for loop to iterate string
+dictionary to store string and its length
+
+How to do:
+split the string --> it will turn into List
+iterate the list
+In dictionary, key=string value and value=size
+loop over the dictionary to find the max value
 '''
+
+str='python programming is fun'
+lst=str.split()
+
+dict={}
+
+for word in lst:
+    length=len(word)
+    dict.__setitem__(word,length)
+
+print(dict.items())
+size=0
+str2=''
+for key,value in dict.items():
+    if value>size:
+        size=value
+        str2=key
+print(str2,':',size)
+
+
+
+
+
