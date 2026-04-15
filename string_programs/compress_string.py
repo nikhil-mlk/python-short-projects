7e4306e8702c1e90076f78015914f14d9b7b7da2
nikhil-mlk <Nikhil.mlk19861986@gmail.com>
2026-04-14 22:04:43 -0700

    Initial commit

---
+++ string_programs/compress_string.py
@@ -0,0 +1,27 @@
+'''
+Input: "aaabbca"
+Break it into consecutive groups:
+"aaa" → a3
+"bb" → b2
+"c" → c1
+"a" → a1 (this is separate from the first a group!)
+
+Output: "a3b2c1a1"
+
+Idea:
+iterate string and place characters in dictionary along with their values (number of time occurence)
+
+'''
+str='aaabbca'
+final_str=''
+
+i=str[0]
+j=str[0]
+
+k=0
+while k<len(str):
+    pass
+
+
+
+
