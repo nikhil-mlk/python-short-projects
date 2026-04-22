ed1e344a99205a3a11b0c16c3413d971d572bb92
nikhil-mlk <Nikhil.mlk19861986@gmail.com>
2026-04-21 21:06:03 -0700

    Second commit

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -15,17 +15,23 @@ Working On:
 for choice 2: Need to check if file is empty then return message if nothing in file
 handle the situation where user coming for the first time and instead of adding credentials he/she try to view file
 '''
+from unittest import case
 
+import sys
 master_record={}
+master_record.__setitem__('nmalik','1234')
+master_record.__setitem__('niks1986','4321')
+
 def master_user_registration():
-    user_name=input('Enter master username: ')
-    if user_name in master_record.keys():
+    master_user_name=input('Enter master username: ')
+    if master_user_name in master_record.keys():
         print('Username already exists. Please try with different username.')
         master_user_registration()
     else:
-        user_password = input('Enter master password: ')
-        master_record.__setitem__(user_name,user_password)
+        master_password = input('Enter master password: ')
+        master_record.__setitem__(master_user_name,master_password)
         print('Username and password successfully registered.')
+    validate_user_and_create_file()
 
 def validate_user_and_create_file():
     user=input('Enter Master username: ')
@@ -42,17 +48,34 @@ def validate_user_and_create_file():
             print('Access Granted !!!!')
             print('Now You can start storing your credentials')
     while True:
-        choice=input('Do you want to add New Credentials or want to quit? --> 1->  Add Credentials and 2->: View Credentials and 3-> Exit: ')
-        if choice=='1':
-            user_n = input('Enter new User Name: ')
-            user_p = input('Enter new password: ')
-            with open(user+'.txt','a') as f:
-                f.write(user_n+' | '+user_p+'\n')
-        elif choice=='2':
-            pass
-        elif choice=='3':
-            print('--- Thank you for choosing Password Management application -----')
-            break
+        choice=int(input('What you want to do next? : 1->  Add Credentials and 2->: View Credentials and 3-> Exit: '))
+        match choice:
+            case 1:
+                user_n = input('Enter new User Name: ')
+                user_p = input('Enter new password: ')
+                with open(user+'.txt', 'a') as f:
+                    f.write(user_n + ' | ' + user_p + '\n')
+            case 2:
+                try:
+                    with open(user+'.txt', 'r') as f:
+                        length_of_text = len(f.read())
+                        if length_of_text == 0:
+                            print('No records found')
+                        else:
+                            print(f.read())
+                except FileNotFoundError:
+                    print('File Not found')
+
+            case 3:
+                print('--- Thank you for choosing Password Management application -----')
+                sys.exit()
+
+            case _:
+                print('Invalid Input')
+
+
+master_user_registration()
+
 
 
 
