b4874d0f2b9ff0440f9033c746c4eb015b0d7323
nikhil-mlk <nikhil.mlk19861986@gmail.com>
2026-06-15 08:08:38 -0700

    Committing final changes to QR Code generator project

---
+++ atm_machine/atm_machine.py
@@ -0,0 +1,39 @@
+'''
+Objective
+Create ATM system where user can intract with accounts
+
+Main Modules:
+1. Add Customer
+2. Deposit Money
+3. Withdraw Money
+4. Change pin
+5. Mini Statement
+6. Exit
+
+Add Customer:
+- ask Name
+- ask username (unique)
+- ask pin
+- Deposit initial money
+- Account Number should be created
+
+Deposit Money:
+- amount should be > 0
+- balance should be update
+
+Withdraw Money:
+- Amount > 0
+- Sufficient balance should present in account
+
+Change Pin:
+Pin should be updated in Excel
+
+Mini Statement:
+Record every transaction
+Deposited $2000
+Withdrawn $1000
+Deposited $500
+
+Exit:
+Exit the system
+'''
